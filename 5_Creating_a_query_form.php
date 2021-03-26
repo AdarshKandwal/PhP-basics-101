a6d565daf34e58ae978c86155e9b947465da821e
Adarsh kandwal <41911887+AdarshKandwal@users.noreply.github.com>
2021-03-26 13:39:02 +0530

    Add files via upload

---
+++ 5_Creating_a_query_form.php
@@ -0,0 +1,120 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Features</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Pricing</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<?php
+
+	if($_SERVER["REQUEST_METHOD"]=='POST'){
+
+		$email=$_POST['email'];
+		$desc=$_POST['desc'];
+
+    $servername="localhost";
+    $username="root";
+    $password="";
+    $database="myfisrtdatabase";
+
+    // create a connection
+    $conn=mysqli_connect($servername,$username,$password,$database);
+
+    // dir if connection was not successful
+
+    if(!$conn){
+      die("not connected");
+    }
+    else{
+      echo "Connection was succesfull";
+    }
+
+    $sql="INSERT INTO `queries` (`Email`,`Description`) VALUES ('$email','$desc')";
+
+    $result=mysqli_query($conn,$sql);
+
+    // Check for the table creation success
+
+    if($result){
+     echo '<div class="alert alert-warning alert-dismissible fade show" role="alert">
+  <strong>Data enterd </strong> You should check in on some of those fields below.
+  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+</div>';
+  } 
+    }
+
+    else{
+      echo "Not entered:".mysqli_error($conn);
+    }
+  }
+
+	
+
+
+?>
+
+<div class="container mt-3">
+<form action="/learn/5_Creating_a_query_form.php" method="post">
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Email address</label>
+    <input type="email" name="email" class="form-control" id="email" aria-describedby="emailHelp">
+    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+  </div>
+    <div class="mb-3">
+          <label for="desc">Description</label>
+          <textarea class="form-control" name="desc" id="desc" cols="30" rows="10"></textarea>
+  </div>
+
+  
+  <div class="mb-3 form-check">
+ 
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+</div>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.6.0/dist/umd/popper.min.js" integrity="sha384-KsvD1yqQ1/1+IA7gi3P0tyJcT3vR+NdBTt13hSJ2lnve8agRGXTTyNaBYmCR/Nwi" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.min.js" integrity="sha384-nsg8ua9HAw1y0W1btsyWgBklPnCUAFLuTMS2G72MMONqmOymq585AcH49TLBQObG" crossorigin="anonymous"></script>
+    -->
+
+
+
+    
+  </body>
+</html>
